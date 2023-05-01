c59dc66a548580975227c5b0a9146349c0359a54
Anastasiia Savenok <i.wasber@gmail.com>
2023-05-01 20:20:56 +0300

    add Statistics

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,14 @@
 import { Container } from './App.styled';
-import Section from '../Section/Section';
+import Section from 'components/Section/Section';
+import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
+import Statistics from 'components/Statictics/Statistics';
 
 export default function App() {
   return (
     <Container>
       <Section title="">
         <FeedbackOptions options={} onLeaveFeedback={}></FeedbackOptions>
+        <Statistics good={} neutral={} bad={} total={} positivePercentage={}></Statistics>
       </Section>
     </Container>
   );
