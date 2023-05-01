6c0ab3e336c64f262f95158ad3bd29ce775ad0fe
Anastasiia Savenok <i.wasber@gmail.com>
2023-05-01 20:26:05 +0300

    add Notification

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,14 @@ import { Container } from './App.styled';
 import Section from 'components/Section/Section';
 import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
 import Statistics from 'components/Statictics/Statistics';
+import Notification from 'components/Notification/Notification';
 
 export default function App() {
   return (
     <Container>
       <Section title="">
         <FeedbackOptions options={} onLeaveFeedback={}></FeedbackOptions>
+        <Notification message="There is no feedback"></Notification>
         <Statistics good={} neutral={} bad={} total={} positivePercentage={}></Statistics>
       </Section>
     </Container>
